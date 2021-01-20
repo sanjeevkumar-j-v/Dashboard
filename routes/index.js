a8199fefa5be765c1c2a97257501f4789463fd7b
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2021-01-20 13:28:08 +0530

    use lower case for view file names

--- routes/index.js
+++ routes/index.js
@@ -7,33 +7,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/profile", function (req, res, next) {
-  res.render("Profile", { title: "Profile | Club Inno", page: "profile" });
+  res.render("profile", { title: "Profile | Club Inno", page: "profile" });
 });
 
 router.get("/dashboard", function (req, res, next) {
-  res.render("Dashboard", { title: "Dashboard | Club Inno", page: "dashboard" });
+  res.render("pashboard", { title: "Dashboard | Club Inno", page: "dashboard" });
 });
 
 router.get("/ongoing-event", function (req, res, next) {
-  res.render("Ongoing Event", { title: "Event | Club Inno", page: "event" });
+  res.render("ongoing event", { title: "Event | Club Inno", page: "event" });
 });
 router.get("/upcoming-event", function (req, res, next) {
-  res.render("Upcoming Event", { title: "Event | Club Inno", page: "event" });
+  res.render("upcoming event", { title: "Event | Club Inno", page: "event" });
 });
 router.get("/past-event", function (req, res, next) {
-  res.render("Past Event", { title: "Event | Club Inno", page: "event" });
+  res.render("past event", { title: "Event | Club Inno", page: "event" });
 });
 
 router.get("/office-bearers", function (req, res, next) {
-  res.render("Office Bearers", { title: "OB | Club Inno", page: "office-bearers" });
+  res.render("office bearers", { title: "OB | Club Inno", page: "office-bearers" });
 });
 
 router.get("/contact", function (req, res, next) {
-  res.render("Contact", { title: "Contact | Club Inno", page: "contact" });
+  res.render("contact", { title: "Contact | Club Inno", page: "contact" });
 });
 
 router.get("/admin", function (req, res, next) {
-  res.render("Admin", { title: "Admin | Club Inno", page: "admin" });
+  res.render("admin", { title: "Admin | Club Inno", page: "admin" });
 });
 
 
