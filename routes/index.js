c1f08c3f8990bd16dc16a2271d91a557011ad9d9
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2021-01-20 13:17:28 +0530

    link all pages

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,40 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home | Club Inno' });
+  res.render("index", { title: "Home | Club Inno", page: "home" });
+});
+
+router.get("/profile", function (req, res, next) {
+  res.render("Profile", { title: "Profile | Club Inno", page: "profile" });
 });
 
 router.get("/dashboard", function (req, res, next) {
-  res.render("index", { title: "Dashboard | Club Inno" });
+  res.render("dashboard", { title: "Dashboard | Club Inno", page: "dashboard" });
 });
+
+router.get("/ongoing-event", function (req, res, next) {
+  res.render("ongoing event", { title: "Event | Club Inno", page: "event" });
+});
+router.get("/upcoming-event", function (req, res, next) {
+  res.render("Upcoming Event", { title: "Event | Club Inno", page: "event" });
+});
+router.get("/past-event", function (req, res, next) {
+  res.render("Past Event", { title: "Event | Club Inno", page: "event" });
+});
+
+router.get("/office-bearers", function (req, res, next) {
+  res.render("Office Bearers", { title: "OB | Club Inno", page: "office-bearers" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("Contact", { title: "Contact | Club Inno", page: "contact" });
+});
+
+router.get("/admin", function (req, res, next) {
+  res.render("Admin", { title: "Admin | Club Inno", page: "admin" });
+});
+
+
+router.use("/users", require('./users'));
 
 module.exports = router;
